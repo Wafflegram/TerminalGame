241693c7645d17e5a0c9e22efe9da349f9d34e89
Wafflegram <evandagamer1267@gmail.com>
2021-11-03 19:45:14 -0700

    Added menu feature to choose apps, although only 1 doesn't result in an error. Null can be fixed later in the game

--- CALSIGN.py
+++ CALSIGN.py
@@ -5,6 +5,9 @@ on_vulture = False
 null_fix = False
 on_null = False
 
+class FileCorruption(Exception):
+    pass
+
 def sys_start():
     print("(**%PROTOCAL01011010%**)")
     print("[0][1][EXEC START][1][0]")
@@ -31,5 +34,29 @@ user_key = input("Please enter user key: ")
 
 if user_key == "Telebeetle22":
     print("Access granted")
+    on_menu = True
 else:
     sys_lock()
+
+apps_list = ["Vulture Console", "null", "Unknown", "Notepad"]
+
+while on_menu == True:
+    print("Available Apps: " + str(apps_list))
+    app_chosen = input("Choose an app: ")
+
+    if app_chosen == "Vulture Console":
+        print("Opening VULTURE")
+    elif app_chosen == "null":
+        if null_fix == True:
+            pass
+        else:
+            print("_________________________________________________________SYSTEM SHUTDOWN_________________________________________________________")
+            print("")
+            print("##################################################0x00000012 TRAP_CAUSE_UNKNOWN##################################################")
+            print("_________________________________________________________________________________________________________________________________")
+            print("")
+            raise FileCorruption
+    elif app_chosen == "Notepad":
+        print("Notepad was unable to open. STOP CODE: ERROR_OPEN_FAILED")
+    else:
+        print("App is either unavailable or you spelled it wrong. Case sensitive")
