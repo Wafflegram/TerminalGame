6348e12d7bedfff08fcaabb30539099670320e3d
Wafflegram <evandagamer1267@gmail.com>
2021-11-03 18:43:24 -0700

    Fixing error where functions don't appear in GitHub

--- CALSIGN.py
+++ CALSIGN.py
@@ -1,11 +1,21 @@
-print("(**%PROTOCAL01011010%**)")
-print("[0][1][EXEC START][1][0]")
-print("")
-print("")
-print("EMTECH EST 1984")
-print("")
-print("<|||||||||||||>")
-print("_______________")
-print("")
-print("CALSIGN OS V3.4")
-print("_____________________________________________")
+import sys
+
+on_menu = False
+on_vulture = False
+null_fix = False
+on_null = False
+
+def sys_start():
+    print("(**%PROTOCAL01011010%**)")
+    print("[0][1][EXEC START][1][0]")
+    print("")
+    print("")
+    print("EMTECH EST 1984")
+    print("")
+    print("<|||||||||||||>")
+    print("_______________")
+    print("")
+    print("CALSIGN OS V3.4")
+    print("_____________________________________________")
+
+sys_start()
